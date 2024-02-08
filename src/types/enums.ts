a149515243dc486250b51c0623da741698ff7c21
Brendan Moran <b.daniel.moran@gmail.com>
2024-02-08 13:20:07 -0500

    Remove unnecessary daylight saving time entries in TimeZones enum

--- src/types/enums.ts
+++ src/types/enums.ts
@@ -32,20 +32,13 @@ export const enum TimeZones {
     // United States Time Zones with standard and daylight saving time
     HAST = -10, // Hawaii-Aleutian Standard Time (UTC-10 hours)
     AKST = -9,  // Alaska Standard Time (UTC-9 hours)
-    AKDT = -8,  // Alaska Daylight Time (UTC-8 hours)
     PST = -8,   // Pacific Standard Time (UTC-8 hours)
-    PDT = -7,   // Pacific Daylight Time (UTC-7 hours)
     MST = -7,   // Mountain Standard Time (UTC-7 hours) - Note: Arizona does not observe DST except for the Navajo Nation
-    MDT = -6,   // Mountain Daylight Time (UTC-6 hours)
     CST = -6,   // Central Standard Time (UTC-6 hours)
-    CDT = -5,   // Central Daylight Time (UTC-5 hours)
     EST = -5,   // Eastern Standard Time (UTC-5 hours)
-    EDT = -4,   // Eastern Daylight Time (UTC-4 hours)
 
     // Other Time Zones
     GMT = 0,    // Greenwich Mean Time (UTC+0 hours) - Dublin, Ireland (Note: DST is observed here as Irish Standard Time, IST)
-    BST = 1, // British Summer Time
     UTC = 0,    // Coordinated Universal Time (UTC+0 hours) - Reykjavik, Iceland
-    UTC_PLUS_1 = 1, // UTC+1 is a fixed time zone without DST
     GST = 4,    // Gulf Standard Time (UTC+4 hours) - Dubai and Abu Dhabi (Note: DST is not observed in these locations)
 }
