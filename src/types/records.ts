10ff707b03a84278e2132beb2d8541e6b94b0f5b
Brendan Moran <b.daniel.moran@gmail.com>
2024-02-08 17:36:14 -0500

    Update time zone mappings in records.ts

--- src/types/records.ts
+++ src/types/records.ts
@@ -1,12 +1,12 @@
-import { TimeZones } from "./enums";
+import { TimeZone } from "./enums";
 
-export const timeZoneMappings: Record<TimeZones, string> = {
-    [TimeZones.HAST]: "Pacific/Honolulu",
-    [TimeZones.AKST]: "America/Anchorage",
-    [TimeZones.PST]: "America/Los_Angeles",
-    [TimeZones.MST]: "America/Denver", // Note: Arizona would be "America/Phoenix"
-    [TimeZones.CST]: "America/Chicago",
-    [TimeZones.EST]: "America/New_York",
-    [TimeZones.GMT]: "Europe/London",
-    [TimeZones.GST]: "Asia/Dubai",
+export const timeZoneMappings: Record<TimeZone, string> = {
+    [TimeZone.HAST]: "Pacific/Honolulu",
+    [TimeZone.AKST]: "America/Anchorage",
+    [TimeZone.PST]: "America/Los_Angeles",
+    [TimeZone.MST]: "America/Denver", // Note: Arizona would be "America/Phoenix"
+    [TimeZone.CST]: "America/Chicago",
+    [TimeZone.EST]: "America/New_York",
+    [TimeZone.GMT]: "Europe/London",
+    [TimeZone.GST]: "Asia/Dubai",
   };
