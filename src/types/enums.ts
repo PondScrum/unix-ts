3dc2fa7cc4060188dc4b4725e764b781540f5980
Brendan Moran <b.daniel.moran@gmail.com>
2024-02-08 11:28:39 -0500

    Update ISOStringFormats enum with additional date formats

--- src/types/enums.ts
+++ src/types/enums.ts
@@ -20,7 +20,11 @@ export const enum TimeUnit {
 
 //TODO implement common formats
 export const enum ISOStringFormats { 
-    ShortDate = "dd/mm/yy"
+    ShortDate = "YYYY-MM-DD", // ISO format for date only
+    FullDate = "YYYY-MM-DDThh:mm:ss.sssZ", // ISO format for date and time
+    FullDateWithOffset = "YYYY-MM-DDThh:mm:ss.sss±hh:mm", // ISO format with time zone offset
+    WeekDate = "YYYY-Www-D", // ISO format for week date
+    OrdinalDate = "YYYY-DDD" // ISO format for ordinal date (day of the year)
 }
 
 //TODO implement common formats
