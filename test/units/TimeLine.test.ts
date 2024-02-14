54a2e8de494ef670629940438e1bfb82052364b2
Brendan Moran <b.daniel.moran@gmail.com>
2024-02-14 11:28:53 -0500

    Add unit tests for insert, pop, and push methods in TimeLine class

--- test/units/TimeLine.test.ts
+++ test/units/TimeLine.test.ts
@@ -58,3 +58,52 @@ test('Test sort desc', ()=>{
        si = event.metaData.sort_index_end;
     });
 })
+
+test('insert method should correctly insert an event into the timeline', () => {
+    // Initialize timeline with events
+    let t1 = new TimeLine(gen_arr());
+    
+    // Create a new event to insert
+    let newEvent = new TimedEvent(new Epoch(ts_start + 10_000), new Epoch(ts_start + 20_000), {sort_index_start: 1.5, sort_index_end: 1.5});
+
+    // Insert new event
+    t1.insert(newEvent);
+    
+    // After insertion, the new event should be at index 2
+    expect(t1.events[2]).toBe(newEvent);
+});
+
+test('pop method should remove the last or first event from the timeline', () => {
+    // Initialize timeline with events
+    let t1 = new TimeLine(gen_arr());
+    
+    // Test pop with default direction (right)
+    let rightPop = t1.pop();
+    // The event that was popped should have sort_index_start of 4 (the last one)
+    expect(rightPop.metaData.sort_index_start).toBe(4);
+
+    // Reset and test pop with direction left
+    t1 = new TimeLine(gen_arr());
+    let leftPop = t1.pop('left');
+    // The event that was popped should have sort_index_start of 0 (the first one)
+    expect(leftPop.metaData.sort_index_start).toBe(0);
+});
+
+test('push method should add an event to the correct end of the timeline based on direction', () => {
+    // Initialize timeline with events
+    let t1 = new TimeLine(gen_arr());
+    
+    // Create new events to push
+    let newEventRight = new TimedEvent(new Epoch(ts_start + 500_000), new Epoch(ts_start + 600_000), {sort_index_start: 5, sort_index_end: 5});
+    let newEventLeft = new TimedEvent(new Epoch(ts_start - 100_000), new Epoch(ts_start), {sort_index_start: 0, sort_index_end: 0});
+
+    // Push new event to the right
+    t1.push(newEventRight, 'right');
+    // The new event should be the last one in the timeline
+    expect(t1.events[t1.events.length - 1]).toBe(newEventRight);
+
+    // Push new event to the left
+    t1.push(newEventLeft, 'left');
+    // The new event should be the first one in the timeline
+    expect(t1.events[0]).toBe(newEventLeft);
+});
